bddac7cabc16f7806652de6eae47e7d3b22d4516
Matheus Oliveira <okmatheusoliveira@gmail.com>
2023-02-11 14:35:13 -0300

    Added create table statement.

---
+++ SQL/create-table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE EmployeeDemographics(
+EmployeeID INT,
+FirstName VARCHAR(50),
+LastName VARCHAR(50),
+Age INT,
+Gender VARCHAR(50)
+);
+
+CREATE TABLE EmployeeSalary(
+EmployeeID INT,
+JobTitle VARCHAR(50),
+Salary INT
+);
