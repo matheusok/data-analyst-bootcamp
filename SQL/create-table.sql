cdd45bfbdf61772910887c34acc4db1ecfe9f1e0
Matheus Oliveira <okmatheusoliveira@gmail.com>
2023-02-11 17:39:59 -0300

    Added the warehouse employee demographics table.

--- SQL/create-table.sql
+++ SQL/create-table.sql
@@ -11,3 +11,11 @@ EmployeeID INT,
 JobTitle VARCHAR(50),
 Salary INT
 );
+
+CREATE TABLE WareHouseEmployeeDemographics 
+(EmployeeID INT, 
+FirstName VARCHAR(50), 
+LastName VARCHAR(50), 
+Age INT, 
+Gender VARCHAR(50)
+);
