6ef96b2b00d863ed7b0529d673cf0d16bb34a530
Matheus Oliveira <okmatheusoliveira@gmail.com>
2023-02-11 17:10:21 -0300

    Added some inserts into tables.

--- SQL/insert-into.sql
+++ SQL/insert-into.sql
@@ -21,3 +21,14 @@ VALUES
 (1007, 'Supplier Relations', 41000),
 (1008, 'Salesman', 48000),
 (1009, 'Accountant', 42000);
+
+INSERT INTO EmployeeDemographics
+VALUES
+(1011, 'Ryan', 'Howard', 26, 'Male'),
+(NULL, 'Holly','Flax', NULL, 'Male'),
+(1013, 'Darryl', 'Philbin', NULL, 'Male');
+
+INSERT INTO EmployeeSalary
+VALUES
+(1010, NULL, 47000),
+(NULL, 'Salesman', 43000);
