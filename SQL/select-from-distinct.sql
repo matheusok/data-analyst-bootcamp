eb0452b874b8d8e739c19750e521c5a5acc18682
Matheus Oliveira <okmatheusoliveira@gmail.com>
2023-02-11 14:39:32 -0300

    Added select, from and distinct statements.

---
+++ SQL/select-from-distinct.sql
@@ -0,0 +1,14 @@
+SELECT *
+FROM EmployeeDemographics;
+
+SELECT *
+FROM EmployeeSalary;
+
+SELECT *
+FROM SQLDataAnalystBootcamp.dbo.EmployeeSalary;
+
+SELECT DISTINCT (EmployeeID)
+FROM EmployeeDemographics;
+
+SELECT DISTINCT (Gender)
+FROM EmployeeDemographics;
